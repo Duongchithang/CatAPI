5237d6dfad110cc05ff25007b72cf255dd8cbbac
Dương Chí Thắng <Chithang0210@gmail.com>
2022-10-13 15:59:19 +0700

    update code message

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <router-view/>
+  <div class="flex justify-center items-center">
+    <router-view/>
+  </div>
 </template>
-
 <style>
-#app {
+#app{
   font-family: 'Montserrat', sans-serif;
-  max-width: 1300px;
+  max-width: 1500px;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  margin-right: 0;
 }
 </style>
